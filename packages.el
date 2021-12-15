27c3d9a7871b3cd815ab422d42eff3c3980f0c71
WindProphet <lihanyuan1996@gmail.com>
2021-12-15 09:31:02 +0900

    add package.el condition for installing package

--- packages.el
+++ packages.el
@@ -1,66 +1,24 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here and run 'doom sync'
-;; on the command line, then restart Emacs for the changes to take effect -- or
-;; use 'M-x doom/reload'.
-
-
-;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
-
-(package! sis)
-(package! telega
-  :recipe (:files (:defaults "contrib/*.el" "etc" "server" "Makefile")))
-(package! mastodon
-  :recipe (:host github :repo "WindProphet/mastodon.el"))
-(package! which-key-posframe)
-(package! vertico-posframe)
-(package! eldoc-posframe
-  :recipe (:host github :repo "WindProphet/eldoc-posframe"))
-(package! dash-at-point
-  :recipe (:host github :repo "stanaka/dash-at-point"))
+(when (featurep! :app telegram)
+  (package! telega
+    :recipe (:files (:defaults "contrib/*.el" "etc" "server" "Makefile"))))
+(when (featurep! :app mastodon)
+  (package! mastodon
+    :recipe (:host github :repo "WindProphet/mastodon.el")))
+(when (featurep! :ui whichkey +childframe)
+  (package! which-key-posframe))
+(when (featurep! :completion vertico +childframe)
+  (package! vertico-posframe))
+(when nil
+  (package! eldoc-posframe
+    :recipe (:host github :repo "WindProphet/eldoc-posframe")))
+(when IS-MAC
+  (package! dash-at-point
+    :recipe (:host github :repo "stanaka/dash-at-point")))
 (package! devdocs
   :recipe (:host github :repo "astoff/devdocs.el"))
 (when (featurep! :lang web)
   (package! graphql-mode))
 
-;; To install a package directly from a remote git repo, you must specify a
-;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
-;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
-
-;; If the package you are trying to install does not contain a PACKAGENAME.el
-;; file, or is located in a subdirectory of the repo, you'll need to specify
-;; `:files' in the `:recipe':
-;(package! this-package
-;  :recipe (:host github :repo "username/repo"
-;           :files ("some-file.el" "src/lisp/*.el")))
-
-;; If you'd like to disable a package included with Doom, you can do so here
-;; with the `:disable' property:
-;(package! builtin-package :disable t)
-
-;; You can override the recipe of a built in package without having to specify
-;; all the properties for `:recipe'. These will inherit the rest of its recipe
-;; from Doom or MELPA/ELPA/Emacsmirror:
-;(package! builtin-package :recipe (:nonrecursive t))
-;(package! builtin-package-2 :recipe (:repo "myfork/package"))
-
-;; Specify a `:branch' to install a package from a particular branch or tag.
-;; This is required for some packages whose default branch isn't 'master' (which
-;; our package manager can't deal with; see raxod502/straight.el#279)
-;(package! builtin-package :recipe (:branch "develop"))
-
-;; Use `:pin' to specify a particular commit to install.
-;(package! builtin-package :pin "1a2b3c4d5e")
-
-
-;; Doom's packages are pinned to a specific commit and updated from release to
-;; release. The `unpin!' macro allows you to unpin single packages...
-;(unpin! pinned-package)
-;; ...or multiple packages
-;(unpin! pinned-package another-pinned-package)
-;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
-;(unpin! t)
