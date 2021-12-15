b0b66ed1a535a1534ac682d8ecbc36ac4a801cd5
WindProphet <lihanyuan1996@gmail.com>
2021-12-15 09:30:40 +0900

    add graphql support

--- packages.el
+++ packages.el
@@ -22,6 +22,8 @@
   :recipe (:host github :repo "stanaka/dash-at-point"))
 (package! devdocs
   :recipe (:host github :repo "astoff/devdocs.el"))
+(when (featurep! :lang web)
+  (package! graphql-mode))
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
