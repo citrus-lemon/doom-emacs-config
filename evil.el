4e6cefcc41ce500a10ff7aac180dda192de9abc0
WindProphet <lihanyuan1996@gmail.com>
2021-12-15 09:23:44 +0900

    bind C-e for scrolling in evil-mode

--- evil.el
+++ evil.el
@@ -2,9 +2,13 @@
 
 (when (featurep 'evil)
   (setq display-line-numbers-type 'relative)
+  (setq evil-want-C-u-scroll nil)
+  (map! :map evil-motion-state-map
+        "C-e" #'evil-scroll-up)
   (map! :map evil-insert-state-map
         "C-n" nil
-        "C-p" nil)
+        "C-p" nil
+        "C-y" #'yank)
   (map! :leader
         :when (featurep! :ui workspaces)
         :prefix ("TAB" . "workspace")
