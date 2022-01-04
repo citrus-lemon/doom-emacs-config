92caaeed954c983221d7e1b01413f45203e7a75b
WindProphet <lihanyuan1996@gmail.com>
2022-01-04 09:04:31 +0900

    remov term in util.el

--- util.el
+++ util.el
@@ -1,18 +1,5 @@
 ;;; util.el -*- lexical-binding: t; -*-
 
-(defun my/new-terminal ()
-  "Create a terminal in a new buffer"
-  (interactive)
-  (let ((buffer (generate-new-buffer "Terminal"))
-        (dir (projectile-project-p)))
-    (switch-to-buffer buffer)
-    (if dir (cd dir))
-    (vterm-mode)
-    (setq-local vterm-buffer-name-string "Term: %s")
-    (setq mode-line-format (default-value 'mode-line-format))
-    buffer))
-(defalias '=term 'my/new-terminal)
-
 (defun my/copy ()
   (interactive)
   (if mark-active
