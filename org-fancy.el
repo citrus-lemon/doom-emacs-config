fbf1494bb7257306aa05b8c01d9d5eaf13c0d367
WindProphet <lihanyuan1996@gmail.com>
2022-02-02 19:55:55 +0900

    add org style of begin_src

--- org-fancy.el
+++ org-fancy.el
@@ -34,9 +34,96 @@
  '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
  '(org-drawer ((t (:inherit fixed-pitch))))
  '(org-superstar-header-bullet ((t (:family "LXGW WenKai" :inherit fixed-pitch))))
- '(org-block-begin-line ((t (:inherit (shadow fixed-pitch)))))
+ '(org-block-begin-line ((t (:foreground nil :slant normal :inherit (shadow fixed-pitch)))))
  '(org-block-end-line ((t (:inherit (shadow fixed-pitch)))))
  '(org-todo ((t (:inherit fixed-pitch))))
  '(org-done ((t (:inherit fixed-pitch :foreground "#000000")))))
+
+;; sources code block. from https://github.com/zzamboni/dot-emacs/blob/master/init.org#source-code-blocks
+
+(with-eval-after-load 'org
+  (defvar-local rasmus/org-at-src-begin -1
+    "Variable that holds whether last position was a ")
+
+  (defvar rasmus/ob-header-symbol ?☰
+    "Symbol used for babel headers")
+
+  (defun rasmus/org-prettify-src--update ()
+    (let ((case-fold-search t)
+          (re "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*")
+          found)
+      (save-excursion
+        (goto-char (point-min))
+        (while (re-search-forward re nil t)
+          (goto-char (match-end 0))
+          (let ((args (org-trim
+                       (buffer-substring-no-properties (point)
+                                                       (line-end-position)))))
+            (when (org-string-nw-p args)
+              (let ((new-cell (cons args rasmus/ob-header-symbol)))
+                (cl-pushnew new-cell prettify-symbols-alist :test #'equal)
+                (cl-pushnew new-cell found :test #'equal)))))
+        (setq prettify-symbols-alist
+              (cl-set-difference prettify-symbols-alist
+                                 (cl-set-difference
+                                  (cl-remove-if-not
+                                   (lambda (elm)
+                                     (eq (cdr elm) rasmus/ob-header-symbol))
+                                   prettify-symbols-alist)
+                                  found :test #'equal)))
+        ;; Clean up old font-lock-keywords.
+        (font-lock-remove-keywords nil prettify-symbols--keywords)
+        (setq prettify-symbols--keywords (prettify-symbols--make-keywords))
+        (font-lock-add-keywords nil prettify-symbols--keywords)
+        (while (re-search-forward re nil t)
+          (font-lock-flush (line-beginning-position) (line-end-position))))))
+
+  (defun rasmus/org-prettify-src ()
+    "Hide src options via `prettify-symbols-mode'.
+        `prettify-symbols-mode' is used because it has uncollpasing. It's
+        may not be efficient."
+    (let* ((case-fold-search t)
+           (at-src-block (save-excursion
+                           (beginning-of-line)
+                           (looking-at "^[ \t]*#\\+begin_src[ \t]+[^ \f\t\n\r\v]+[ \t]*"))))
+      ;; Test if we moved out of a block.
+      (when (or (and rasmus/org-at-src-begin
+                     (not at-src-block))
+                ;; File was just opened.
+                (eq rasmus/org-at-src-begin -1))
+        (rasmus/org-prettify-src--update))
+      ;; Remove composition if at line; doesn't work properly.
+      ;; (when at-src-block
+      ;;   (with-silent-modifications
+      ;;     (remove-text-properties (match-end 0)
+      ;;                             (1+ (line-end-position))
+      ;;                             '(composition))))
+      (setq rasmus/org-at-src-begin at-src-block)))
+
+  ;; This function helps to produce a single glyph out of a
+  ;; string. The glyph can then be used in prettify-symbols-alist.
+  ;; This function was provided by Ihor in the org-mode mailing list.
+  (defun yant/str-to-glyph (str)
+    "Transform string into glyph, displayed correctly."
+    (let ((composition nil))
+      (dolist (char (string-to-list str)
+                    (nreverse (cdr composition)))
+        (push char composition)
+        (push '(Br . Bl) composition))))
+
+  (defun rasmus/org-prettify-symbols ()
+    (mapc (apply-partially 'add-to-list 'prettify-symbols-alist)
+          (cl-reduce 'append
+                     (mapcar (lambda (x) (list x (cons (upcase (car x)) (cdr x))))
+                             `(("#+begin_src" . ?⎡) ;; ⎡ ➤ 🖝 ➟ ➤ ✎
+                               ;; multi-character strings can be used with something like this:
+                               ;; ("#+begin_src" . ,(yant/str-to-glyph "```"))
+                               ("#+end_src"   . ?⎣) ;; ⎣ ✐
+                               ("#+header:" . ,rasmus/ob-header-symbol)
+                               ("#+begin_quote" . ?«)
+                               ("#+end_quote" . ?»)))))
+    (turn-on-prettify-symbols-mode)
+    (add-hook 'post-command-hook 'rasmus/org-prettify-src t t))
+  (add-hook 'org-mode-hook #'rasmus/org-prettify-symbols))
 
 (setq +org-roam-open-buffer-on-find-file nil)
