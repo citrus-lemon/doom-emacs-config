998e9558a651c1cf84107bf1cda8399e86ffa1c5
WindProphet <lihanyuan1996@gmail.com>
2022-03-14 10:41:26 +0900

    add rust lsp config

--- lang.el
+++ lang.el
@@ -2,3 +2,7 @@
 
 (set-formatter! 'swift-format '("swift-format" ("%S" (or buffer-file-name mode-result "")))
   :modes '(swift-mode))
+
+(when (featurep! :lang rust)
+  (setq lsp-rust-analyzer-proc-macro-enable t)
+  (setq lsp-rust-analyzer-server-display-inlay-hints t))
