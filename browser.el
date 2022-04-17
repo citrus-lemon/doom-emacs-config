8841ae892610510009f33e500795353cad5e15b1
WindProphet <lihanyuan1996@gmail.com>
2022-04-17 17:27:07 +0900

    add w3m key binding

--- browser.el
+++ browser.el
@@ -3,5 +3,9 @@
 (use-package! w3m
   :commands (w3m w3m-search)
   :config
+  (map! :leader
+        :prefix ("j" . "Browser")
+        :desc "Search" "j" #'w3m-search-new-session
+        :desc "Start"  "w" #'w3m)
   (setq w3m-search-default-engine "google-en")
   (setq w3m-use-tab-line nil))
