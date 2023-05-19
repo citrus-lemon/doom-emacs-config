1fa39f0eb91529a5fcc8f8ba2bf7895ce8a3dc40
Lemon <lihanyuan1996@gmail.com>
2023-05-19 11:19:32 +0900

    redefine org vars

--- personal.el
+++ personal.el
@@ -7,11 +7,17 @@
 (setq frame-title-format '("%b – Emacs Master"))
 (setq +doom-dashboard-name "*dashboard*")
 
+(defvar icloud-root-directory
+  "~/Library/Mobile Documents/com~apple~CloudDocs/"
+  "iCloud root directory")
+
 (setq org-directory "~/org/")
-(setq org-roam-directory "~/org/")
+;;; if org in iCloud
+;; (setq org-directory (expand-file-name "Documents/Memo/" icloud-root-directory))
+(setq org-roam-directory org-directory)
+(setq org-id-locations-file (expand-file-name ".orgids" org-directory))
 (setq org-agenda-files
-      (directory-files-recursively
-       "~/org" "\\.org$"))
+      (directory-files-recursively org-directory "\\.org$"))
 
 ;; projectile auto projects discovery
 ;; M-x projectile-discover-projects-in-search-path
